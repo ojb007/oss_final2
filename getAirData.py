71a363287e1b4168f066938baed0732170e25429
Dongkyu Kim <pythakyu@gmail.com>
2024-07-10 20:39:54 +0900

    [ADD] filter data by date

--- getAirData.py
+++ getAirData.py
@@ -3,13 +3,13 @@ import requests
 def fetch_data():
     url = 'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst'
     params ={
-        'serviceKey' : 'API_KEY',
+        'serviceKey' : 'MQrOmcyT4jVjtpceKVFlzVAV7g2Aec3Wh1hvr2gw086NcElfzKtX93eY/qT9UG4dsTB/J/VnqjOS0RzSrnLdag==',
         'returnType' : 'json',
         'numOfRows' : '100',
         'pageNo' : '1',
         'itemCode' : 'PM10',
         'dataGubun' : 'DAILY',
-        'searchCondition' : 'WEEK'
+        'searchCondition' : 'MONTH'
         }
     try:
         response = requests.get(url, params)
@@ -20,5 +20,24 @@ def fetch_data():
     except requests.exceptions.RequestException as err:
         print(f"요청 에러 발생: {err}")
 
-fullData = fetch_data()
-print(fullData)
+# fullData = fetch_data()
+# print(fullData)
+
+def filter_data_by_date(data, date):
+    """
+    API에서 받은 데이터 중 특정 날짜에 해당하는 데이터만 필터링하는 함수.
+
+    Args:
+    data (dict): API로부터 받은 전체 데이터.
+    date (str): 필터링할 날짜 (예: '2024-07-09').
+
+    Returns:
+    list: 특정 날짜에 해당하는 데이터 목록.
+    """
+    items = data['response']['body']['items']
+    return [item for item in items if item['dataTime'].startswith(date)]
+
+full_data = fetch_data()  # 전체 데이터 가져오기
+specific_date = '2024-06-10'  # 필터링할 날짜 최근 한달까지 가능
+filtered_data = filter_data_by_date(full_data, specific_date)  # 특정 날짜 데이터 필터링
+print(filtered_data)
